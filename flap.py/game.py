77595ddf7f8ca4505d619f255f574dfe6b2e8e93
mirek <mirek@cnl.sk>
2022-02-01 18:28:13 +0100

    kolizia s hranicami tohto sveta

--- flap.py/game.py
+++ flap.py/game.py
@@ -7,6 +7,7 @@ flappy = Actor('flappy')
 flappy.x = WIDTH / 2
 flappy.y = HEIGHT / 2
 flappy.ay = 0
+flappy.score = 0
 
 upper_pipe = Actor('pipe.upper')
 upper_pipe.left = WIDTH
@@ -20,10 +21,17 @@ def update():
     upper_pipe.x = upper_pipe.x - 1
     if upper_pipe.right <= 0:
         upper_pipe.left = WIDTH
+        flappy.score += 1
+        print(flappy.score)
 
     # collision detection
     if flappy.colliderect(upper_pipe):
         print('Ta hlavou trubku neporazis.')
+        quit()
+
+    # head
+    if flappy.top <= 0 or flappy.bottom >= HEIGHT:
+        print('Z tohto sveta neutečieš.')
         quit()
 
 def draw():
