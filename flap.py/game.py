3728a93dacf1d86c198266a80a83428e0e814ea1
mirek <mirek@cnl.sk>
2022-02-01 18:21:29 +0100

    pajpa sa pohybuje zprava dolava

--- flap.py/game.py
+++ flap.py/game.py
@@ -8,10 +8,23 @@ flappy.x = WIDTH / 2
 flappy.y = HEIGHT / 2
 flappy.ay = 0
 
+upper_pipe = Actor('pipe.upper')
+upper_pipe.left = WIDTH
+
 def update():
+    # update flappy
     flappy.ay = flappy.ay + GRAVITY
     flappy.y = flappy.y + flappy.ay
 
+    # update pipes
+    upper_pipe.x = upper_pipe.x - 1
+    if upper_pipe.right <= 0:
+        upper_pipe.left = WIDTH
+
 def draw():
-    screen.clear()
+    screen.blit('background', (0, 0))
+    upper_pipe.draw()
     flappy.draw()
+
+def on_key_down():
+    flappy.ay = -6.5
