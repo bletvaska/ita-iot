1cb076dbfec3580ec2460e2c0351bc78e657026c
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-02-05 18:20:52 +0100

    drobny refaktoring

--- python/snake.py
+++ python/snake.py
@@ -1,6 +1,16 @@
+#!/usr/bin/env python3
+
 import curses
 import time
 import random
+
+STATE_PLAYING = 0
+STATE_QUIT = 1
+STATE_ATE_HIMSELF = 2
+STATE_ATE_POISON = 3
+STATE_HIT_BORDER = 4
+
+KEY_ESC = 27
 
 
 def print_center(screen, text: str):
@@ -10,31 +20,28 @@ def print_center(screen, text: str):
     screen.addstr(y, x, text)
 
 
-def generate_food(screen, snake):
+def generate_item(screen, items):
     rows, cols = screen.getmaxyx()
+    item = None
 
-    food = (random.randint(1, rows - 2),
-            random.randint(1, cols - 2))
-
-    while food in snake:
-        food = (random.randint(1, rows - 2),
+    # generate item till it's outside of list of items
+    while item is None or item in items:
+        item = (random.randint(1, rows - 2),
                 random.randint(1, cols - 2))
+    
+    return item
 
-    return food
-
-
-KEY_ESC = 27
 
 def snake(screen):
     # init curses
-
     # nacitavanie klaves cez getch() bude neblokujuci
     screen.nodelay(True)
     # skryt kurzor
     curses.curs_set(0)
 
-    key = None
     rows, cols = screen.getmaxyx()
+
+    # hadik
     snake = [
         (8, 10),  # head
         (9, 10),
@@ -45,30 +52,27 @@ def snake(screen):
     # generovanie jedla
     foods = []
     for _ in range(30):
-        food = generate_food(screen, snake)
+        food = generate_item(screen, snake)
         foods.append(food)
 
     # generovanie otravy
     poison = []
     for _ in range(30):
-        item = (random.randint(1, rows - 2),
-                random.randint(1, cols - 2))
-
-        while item in snake or item in food:
-            item = (random.randint(1, rows - 2),
-                    random.randint(1, cols - 2))
-
+        item = generate_item(screen, snake + foods)
         poison.append(item)
 
     # pozicia a smer hadika na zaciatku
-    y = head[0]
-    x = head[1]
     dx = 0
     dy = -1
     speed = 0.5
 
-    while key != KEY_ESC:
+    state = STATE_PLAYING
+
+    # herna slucka
+    while state == STATE_PLAYING:
         # update
+        head = snake[0]
+
         # zistujem, aky klaves bol stlaceny
         key = screen.getch()
 
@@ -85,47 +89,40 @@ def snake(screen):
             elif key == curses.KEY_DOWN:
                 dx = 0
                 dy = 1
+            elif key == KEY_ESC:
+                state = STATE_QUIT
+                break
 
         # aktualizujem polohu hadika
-        y = y + dy
-        x = x + dx
+        head = ( head[0] + dy, head[1] + dx )
 
         # trafil som kapustu?
-        eating = False
-        if (y, x) in foods:
-            eating = True
-            foods.remove((y, x))
-            food = generate_food(screen, snake)
+        if head in foods:
+            foods.remove(head)
+            food = generate_item(screen, snake + poison)
             foods.append(food)
             speed = speed - 0.01
+        else:
+            # ak som netrafil kapustu, tak odstranim chvost (hybem sa)
+            snake.pop()
 
         # trafil som otravu?
-        if (y, x) in poison:
-            print_center(screen, 'Bleah. Ta toto nebola kapusta :-(')
-            screen.refresh()
-            time.sleep(2)
+        if head in poison:
+            state = STATE_ATE_POISON
             break
 
         # trafil hadik o stenu?
-        if y == 0 or y == rows - 1 or x == 0 or x == cols - 1:
-            print_center(screen, 'Hlavou múr neprerazíš.')
-            screen.refresh()
-            time.sleep(2)
+        if head[0] in (0, rows - 1) or head[1] in (0, cols - 1):
+            state = STATE_HIT_BORDER
             break
 
         # zozral hadik sam seba?
-        if (y, x) in snake[1:]:
-            print_center(screen, 'Sám seba nežerem.')
-            screen.refresh()
-            time.sleep(2)
+        if head in snake[1:]:
+            state = STATE_ATE_HIMSELF
             break
 
-        # update hadik
-        # head
-        snake.insert(0, (y, x))
-        # tail
-        if not eating:
-            snake.pop()
+        # posun novu polohu hlavicky na zaciatok zoznamu
+        snake.insert(0, head)
 
         # render
         screen.clear()
@@ -142,11 +139,23 @@ def snake(screen):
             screen.addstr(item[0], item[1], '☠')
 
         # render hadik
-        screen.addstr(snake[0][0], snake[0][1], '⚉')
+        screen.addstr(head[0], head[1], '⚉')
         for part in snake[1:]:
             screen.addstr(part[0], part[1], '⬤')
 
         screen.refresh()
         time.sleep(speed)
 
+    # zobrazenie zaverecnej spravy podla stavu
+    if state == STATE_ATE_POISON:
+        print_center(screen, 'Bleah. Ta toto nebola kapusta :-(')
+    elif state == STATE_HIT_BORDER:
+        print_center(screen, 'Hlavou múr neprerazíš.')
+    elif state == STATE_ATE_HIMSELF:
+        print_center(screen, 'Sám seba nežerem.')
+
+    screen.refresh()
+    time.sleep(2)
+
 curses.wrapper(snake)
+
