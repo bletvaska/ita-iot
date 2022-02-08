ae31cf654598d149aeea96828b1b61c59e1f744f
mirek <mirek@cnl.sk>
2022-02-08 17:56:10 +0100

    opravena syntax JSON-u podla PEP8

--- python/mqtt_example.py
+++ python/mqtt_example.py
@@ -6,19 +6,19 @@ client.connect('broker.hivemq.com', port=1883)
 client.publish('ita/messages', 'hello world (mirek)')
 
 data = {
-    "name":"mirek",
-    "lat":48.730892,
-    "lon":21.245767,
-    "iconColor":"#0000ff",
-    "icon":"home",
-    "ttl":30,
-    "photoUrl":"https://bletvaska.github.io/images/mirek.na.hackathone.jpg",
-    "weblink":{
-        "name":"homepage",
-        "url":"https://bletvaska.github.io/",
-        "target":"_new"
+    "name": "mirek",
+    "lat": 48.730892,
+    "lon": 21.245767,
+    "iconColor": "#0000ff",
+    "icon": "home",
+    "ttl": 30,
+    "photoUrl": "https://bletvaska.github.io/images/mirek.na.hackathone.jpg",
+    "weblink": {
+        "name": "homepage",
+        "url": "https://bletvaska.github.io/",
+        "target": "_new"
     },
-    "label":"mirek is here",
-    "temp":"3.8°C",
-    "hum":"56%"
+    "label": "mirek is here",
+    "temp": "3.8°C",
+    "hum": "56%"
 }
