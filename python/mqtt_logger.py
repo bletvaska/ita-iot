2e79b4752f02d8b1a12b5edc255f6cc03e898edd
mirek <mirek@cnl.sk>
2022-02-15 17:48:55 +0100

    dokonceny mqtt logger

--- python/mqtt_logger.py
+++ python/mqtt_logger.py
@@ -5,8 +5,29 @@ from time import sleep
 import paho.mqtt.client as mqtt
 
 def on_message_received(client, xxx, message):
-    data = message.payload.decode()
-    print(message.topic, data)
+    try:
+        # decode data from message
+        data = json.loads(message.payload.decode())
+
+        # extract data
+        name = data['name']
+        temp = float(data['temperature'])
+        hum = float(data['humidity'])
+        press = float(data['pressure'])
+
+        # prepare log message
+        now = datetime.now()
+        log = f'{now.strftime("%d.%m.%Y %H:%M:%S")};{name};{temp:.2f};{hum:.2f};{press:.0f}'
+        print(log)
+
+        # write data to log file
+        with open('data.csv', 'a') as file:
+            print(log, file=file)
+
+    except Exception as ex:
+        print(f'Error: chybny vstup: {ex}')
+        print(message.payload)
+
 
 # connect to mqtt broker
 client = mqtt.Client()
@@ -14,5 +35,5 @@ client.connect('broker.hivemq.com', port=1883)
 client.on_message = on_message_received
 client.subscribe('ita/meteoservis/#')
 
-# pocuvaj donekonecna (a este dalej)
+# infinite loop
 client.loop_forever()
